7fbf4acb12c32523eea517e5240545cb2605ae85
Christian Franchin <christian@corp.kionux.com.br>
2019-08-08 16:09:51 +0000

    Option to change movimentation
    Signed-off-by: Christian Franchin <christian@corp.kionux.com.br>

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let ctx;
 let circles = [];
 let time;
 let is_sticked = false;
+let is_movement_1 = false;
 
 
 class Circle{
@@ -29,11 +30,29 @@ class Circle{
 
 	move(){
 		if(time){
-			this.actual_y = (this.index/circles.length)*Math.sin((time-this.index*14)/100)*(this.y2 - this.y1)/2 + (this.y1 + this.y2)/2;
-			if(is_sticked)
-				this.actual_x = (this.index / circles.length)*Math.sin((time-this.index*10)/1000)*(this.x2 - this.x1)/2 + (this.x1 + this.x2)/2;
+			let x_boundaries = (this.x2 - this.x1)/2;
+			let y_boundaries = (this.y2 - this.y1)/2;
+			let x_offset = (this.x1 + this.x2)/2;
+			let y_offset = (this.y1 + this.y2)/2;
+
+			let y_forca = 1;
+			let y_movimento = Math.sin((time-this.index*14)/100)*y_boundaries;
+			if(!is_movement_1)
+				y_forca = Math.sin((Math.abs((circles.length) - this.index)/(circles.length))*Math.PI);
 			else
-				this.actual_x = Math.sin((time-this.index*10)/1000)*(this.x2 - this.x1)/2 + (this.x1 + this.x2)/2;
+				y_forca = (this.index/circles.length);
+
+			let x_forca = Math.sin((time-this.index*10)/1000)*x_boundaries;
+			let x_movimento = (this.index / circles.length);
+
+
+			this.actual_y = y_forca * y_movimento + y_offset;
+			// this.actual_y = (this.index/circles.length) * y_movimento + y_offset;
+			
+			if(is_sticked)
+				this.actual_x = x_movimento * x_forca + x_offset;
+			else
+				this.actual_x = x_forca + x_offset;
 
 		}
 	}
@@ -83,6 +102,9 @@ window.addEventListener('keypress', (e)=>{
 	if(e.code == "KeyP"){
 		is_sticked = !is_sticked;
 	}
+	if(e.code == "KeyM"){
+		is_movement_1 = !is_movement_1;
+	}
 })
 
 function draw() {
@@ -123,6 +145,29 @@ function draw() {
 	ctx.beginPath();
 	ctx.fillStyle = is_sticked?"#66D9EF":"#F92672";
 	ctx.fillText(is_sticked?"True":"False", 96, 50);
+	ctx.closePath();
+
+	ctx.beginPath();
+	ctx.fillStyle = "#F8F8F2";
+	ctx.font = "15px Arial";
+	ctx.fillText("M -", 10, 70);
+	ctx.closePath();
+
+	ctx.beginPath();
+	ctx.fillStyle = "#A6E22E";
+	ctx.font = "15px Arial";
+	ctx.fillText("Movement", 36, 70);
+	ctx.closePath();
+
+	ctx.beginPath();
+	ctx.fillStyle = "#F8F8F2";
+	ctx.font = "15px Arial";
+	ctx.fillText("-", 112, 70);
+	ctx.closePath();
+
+	ctx.beginPath();
+	ctx.fillStyle = is_movement_1?"#66D9EF":"#F92672";
+	ctx.fillText(is_movement_1?"True":"False", 122, 70);
 	ctx.closePath();
 
 }
